173e795be3d53a41d297bafedc9b21a79381389c
Connie <connielau@couchpotatoeducation.com>
2022-11-26 11:23:41 +0000

    added fs questions

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs')
+const path = require('path')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input', 
+        name: 'github', 
+        message: 'Please enter your github name?',
+    },
+    // add more questions
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((res) => {
+        console.log('readme pending');
+        writeToFile('README.md', generateMarkdown({ ...res }))
+    })
+}
 
 // Function call to initialize app
 init();
